e7c6580478e5ea1ee688ac5d10c535bf932a7088
btimicer <abtimicer@gmail.com>
2025-06-22 18:29:49 +0300

    am adaugat pagina de prfile cu functionalitatile de update si change password

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -9,7 +9,7 @@ import { User } from '../models/user.model';
 })
 export class ProfileService {
   private API_BASE_URL = 'http://localhost:8082/api/v1';
-  private readonly USER_PROFILE_STORAGE_KEY = 'profile';
+  public readonly USER_PROFILE_STORAGE_KEY = 'profile';
   private readonly BASIC_AUTH_STORAGE_KEY = 'basicAuthHeader';
 
   constructor( private http: HttpClient,private authService: AuthService) { }
@@ -23,7 +23,7 @@ export class ProfileService {
     const headers = this.authService.getAuthHeaders(false);
 
     return this.http.get<User>(`${this.API_BASE_URL}/user/me`,{headers});
-    
+
 
   }
 
