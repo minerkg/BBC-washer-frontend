c02ce57ad0fe1403c583d8f36e5408a1b063f9ec
ZsoldicsBalazs <119108270+ZsoldicsBalazs@users.noreply.github.com>
2025-06-25 11:02:25 +0300

    added 2 new components for admin and employee

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { BookableUnitsComponent } from './components/bookable-units/bookable-uni
 import { MyReservationsComponent } from './components/my-reservations/my-reservations.component';
 import { WasherManagementComponent } from './components/admin/washer-management/washer-management.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { UserManagerComponent } from './components/user-manager/user-manager.component';
+import { ReservationManagerComponent } from './components/reservation-manager/reservation-manager.component';
 
 export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
@@ -15,7 +17,9 @@ export const routes: Routes = [
     {path:'washers', component: BookableUnitsComponent},
     { path: 'my-reservations', component: MyReservationsComponent },
     { path: 'admin/washers', component: WasherManagementComponent },
-    {path: 'my-profile', component: ProfileComponent}
+    {path: 'admin/user-manager', component: UserManagerComponent},
+    {path: 'admin/reservation-manager', component: ReservationManagerComponent},
+  
   ]},
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   // IMPORTANT: We'll add route guards here later to protect routes
